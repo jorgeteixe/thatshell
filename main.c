ed86e5fd953dc66eb3ea4af5b6b32a7ff16bbfda
jorgeteixe <jorgeteixe11@gmail.com>
2020-09-26 16:42:31 +0200

    Fixes comments and adds tokenization

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-/* comment template */
+/** comment template **/
 
-/*
+/**
  * Function: func_name
  * ------------------
  * func def
@@ -14,7 +15,7 @@
  */
 
 
-/*
+/**
  * Function: prompt
  * ------------------
  * prints the prompt in stdout
@@ -26,7 +27,7 @@ void prompt() {
     printf("%s$:", shellName);
 }
 
-/*
+/**
  * Function: input
  * ------------------
  * func def
@@ -39,24 +40,52 @@ void input(char *inStr) {
     fgets(inStr, 100, stdin);
 }
 
+/**
+ * Function: TrocearCadena
+ * ------------------
+ * The following function splits thestring pointed by
+ * cadena (suposedly not null) into a NULL terminated array
+ * of pointers (trozos).
+ *
+ * *cadena: ref to user input (value gets modified and useless)
+ * *trozos[]: ref to null terminated array
+ *
+ * returns: number of tokens
+ */
+int TrocearCadena(char *cadena, char *trozos[]) {
+    int i = 1;
+    if ((trozos[0] = strtok(cadena, " \n\t")) == NULL)
+        return 0;
+    while ((trozos[i] = strtok(NULL, " \n\t")) != NULL)
+        i++;
+    return i;
+}
+
 int main() {
-    /* Reserve 100 bytes for user input */
+    /** Reserves 100 bytes for user input */
     char *inStr = malloc(100);
 
     while (1) {
-        /* Prints shell prompt */
+        /** Prints shell prompt */
         prompt();
 
-        /* Get user input and writes on inStr */
+        /** Gets user input and writes on inStr */
         input(inStr);
 
+        /** !TODO save inStr in history before TrocearCadena is called */
+        /** Tokenizes user input */
+        char *tokens[50];
+        int ntokens = TrocearCadena(inStr, tokens);
 
 
 
-        /* Variable value test output */
-        printf("%s", inStr);
 
-        /* Final operations */
+        /** DEBUG Prints all tokens separated */
+        for (int i = 0; i < ntokens; ++i) {
+            printf("%s\n", tokens[i]);
+        }
+
+        /** Final operations */
         free(inStr);
         exit(1);
     }
