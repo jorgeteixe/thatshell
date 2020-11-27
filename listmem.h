fabc52afbe469f991647186ed37dbc9492cf35db
migueljuncalp <migueljuncalpaz@outlook.com>
2020-11-27 20:09:39 +0100

    modificacion de un error en el memlist.h

--- listmem.h
+++ listmem.h
@@ -6,12 +6,13 @@
 
 
 typedef struct memory_struct * mem_historic;
+typedef struct struct_command command;
 
 mem_historic create_memlist();
 void remove_memlist(mem_historic);
 void insert_in_memlist(mem_historic, void*, int ,char*,char*,char*);
 void remove_from_memlist(mem_historic, int);
-char* read_from_memlist(mem_historic, int);
+command* read_from_memlist(mem_historic, int);
 int n_elements_in_memlist(mem_historic);
 
 
