5d334c0e6112a285cd8302e1d035628475b66f0d
migueljuncalp <migueljuncalpaz@outlook.com>
2020-12-02 18:08:04 +0100

    cambios listmem.c

--- listmem.c
+++ listmem.c
@@ -115,13 +115,13 @@ void unmap_from_memlist(mem_list historic ,int position){
         printf("the list is empty, you cant remove");
     } else {
         if (position == last) {
-            munmap(historic->list[position]->address,historic->list[position]->size);
-            close(atoi(historic->list[position]->param));
+            if (munmap(historic->list[position]->address,historic->list[position]->size)==0);
+                close(atoi(historic->list[position]->param));
             free(historic->list[position]);
             historic->n_elem--;
         } else {
             munmap(historic->list[position]->address,historic->list[position]->size);
-            close(atoi(historic->list[position]->param));
+          ///  close(atoi(historic->list[position]->param));
             free(historic->list[position]);
             for (int i = position; i <= last; ++i) {
                 historic->list[i] = historic->list[i + 1];
